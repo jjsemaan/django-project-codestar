a2c69614563dc293104554ce8eec004f434816b7
jjsemaan <jalal.semaan@parabim.com>
2024-03-07 20:55:09 +0000

    Add initial connection to elaphantsql

--- codestar/settings.py
+++ codestar/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
+    
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q3-^(t-m0s(gm+hx8853(ul7@k56b(+=r$6df=6@5o@6iszner'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-jjsemaan-django-project-ea0taqkr7u.us1.codeanyapp.com', '.herokuapp.com']
 
@@ -74,11 +78,16 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': 
+dj_database_url.parse(os.environ.get("postgres://wkksxuch:j64Oe5rfScLBJxVq9p8z3TJTr3_uie-A@flora.db.elephantsql.com/wkksxuch"))
 }
 
 
