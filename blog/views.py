c4afeab9db536ba3ae7f6c704bb376f961fb1706
jjsemaan <jalal.semaan@parabim.com>
2024-03-10 15:39:01 +0000

    Add class-based home page

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Post
-#from django.http import HttpResponse
+
 
 # Create your views here.
 class PostList(generic.ListView):
     model = Post
-
-#def blog(request):
-#    return HttpResponse("Hello, Blog!")
