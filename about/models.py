52251c0ef462204be591c65a5a77427f2ff8987c
jjsemaan <jalal.semaan@parabim.com>
2024-03-16 14:10:09 +0000

    Add about app

---
+++ about/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+
+
+class About(models.Model):
+    """
+    Stores a single about me text
+    """
+    title = models.CharField(max_length=200)
+    profile_image = CloudinaryField('image', default='placeholder')
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
