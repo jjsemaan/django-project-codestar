6be7b4747cdb0721999abb30459402e5098999df
jjsemaan <jalal.semaan@parabim.com>
2024-03-17 13:36:55 +0000

    Cleanup about models.py

--- about/models.py
+++ about/models.py
@@ -1,9 +1,6 @@
 from django.db import models
-from cloudinary.models import CloudinaryField
-from django.db import models
 
 # Create your models here.
-
 
 
 class About(models.Model):
@@ -17,10 +14,3 @@ class About(models.Model):
 
     def __str__(self):
         return self.title
-
-class YourModel(models.Model):
-    image = CloudinaryField('image')
-    # Add other fields as needed
-
-class YourModel(models.Model):
-    image = models.ImageField(upload_to='images/', default='images/default.jpg')
--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
